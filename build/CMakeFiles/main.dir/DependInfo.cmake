c963961321b6fa795ab6b95ad5f3f08bca8f6555
Adnanjahangir <Adnanjahangir@github.com>
2019-03-19 10:35:10 +0500

    First commit one cpp

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/emumba/Desktop/Mini_Project_Docker/Project/src/Myproject.cpp" "/home/emumba/Desktop/Mini_Project_Docker/Project/build/CMakeFiles/main.dir/src/Myproject.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/archive"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
